656ed85b7e0202da8ed2b1dbecfa9df159bf1fff
T4TU <markkanen.tarmo@gmail.com>
2018-05-20 21:56:42 +0300

    Add "pääsuunnittelija" prefix

--- RK-Core/src/me/t4tu/rkcore/commands/CoreCommands.java
+++ RK-Core/src/me/t4tu/rkcore/commands/CoreCommands.java
@@ -928,6 +928,15 @@ public class CoreCommands implements CommandExecutor {
 								else if (prefix.equals("pääarkkitehti")) {
 									prefix = "&7[«&4Pääarkkitehti&7»]";
 								}
+								else if (prefix.equals("pääarkkitehti")) {
+									prefix = "&7[«&4Pääarkkitehti&7»]";
+								}
+								else if (prefix.equals("pääarkkitehti")) {
+									prefix = "&7[«&4Pääarkkitehti&7»]";
+								}
+								else if (prefix.equals("pääsuunnittelija")) {
+									prefix = "&7[«&4Pääsuunnittelija&7»]";
+								}
 								else if (prefix.equals("pääkehittäjä")) {
 									prefix = "&7[«&4Pääkehittäjä&7»]";
 								}
